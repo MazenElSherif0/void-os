4e5d6542a1cad41d2f252534d251818d1a992e53
MazenElsherif0 <TP088120@mail.apu.edu.my>
2025-11-29 11:30:16 +0800

    VoidOS: Updated to 2024 Version

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 #![no_std]
 #![no_main]
+
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }

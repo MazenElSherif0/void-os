54ab94b2521488973cde8587f8881d683d6d2956
MazenElsherif0 <TP088120@mail.apu.edu.my>
2025-12-01 15:13:25 +0800

    VoidOS: Improved Testing

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,23 @@
 #![no_main]
 #![reexport_test_harness_main = "test_main"]
 
+mod serial;
 mod vga_buffer;
 use core::panic::PanicInfo;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     error!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
@@ -44,18 +55,31 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 
 // Tests //
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial_assertion... ");
-    assert_eq!(1, 1);
-    println!("[ok]");
+    assert_eq!(0, 1);
 }
 
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     exit_qemu(QemuExitCode::Success);
 }

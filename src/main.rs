e2d0918fa5911abbb31530497ce8855a4dbc6bbe
MazenElsherif0 <TP088120@mail.apu.edu.my>
2025-12-01 19:47:24 +0800

    VoidOS: Integrated Testing Suite

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,10 @@
 #![no_main]
 #![reexport_test_harness_main = "test_main"]
 
+use void_os::test_runner;
 mod serial;
 mod vga_buffer;
 use core::panic::PanicInfo;
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    error!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
@@ -35,51 +20,15 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-// Debug Exits
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-// Tests //
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(0, 1);
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    error!("{}", info);
+    loop {}
 }
 
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    void_os::test_panic_handler(info)
 }
--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(void_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use void_os::println;
+use void_os::vga_buffer;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
@@ -13,11 +15,12 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-fn test_runner(tests: &[&dyn Fn()]) {
-    unimplemented!();
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    loop {}
+    void_os::test_panic_handler(info)
 }
